92d20fe91354820ec2557ced7af334a823e87e9e
youthankare-alt <youthankare@gmail.com>
2026-06-20 15:18:52 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-config-provider>
+    <router-view />
+  </el-config-provider>
+</template>
+
+<script setup>
+// App root component
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif;
+  background-color: #f5f7fa;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  min-height: 100vh;
+}
+</style>
