b127bba5363ed0f33b91ccd7a63572193c05d7b5
youthankare-alt <youthankare@gmail.com>
2026-06-20 15:17:57 +0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+---
+
+## 📄 5. `src/main.js`
+
+```javascript
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+app.use(ElementPlus)
+
+// Register all Element Plus icons
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.mount('#app')
